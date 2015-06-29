e4d3075ad0f931e1957b74cc9b50c3c0cdad264c
Andreas Lutro <anlutro@gmail.com>
2015-06-29 09:40:59 +0200

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='russell',
     packages=['russell'],
-    version='0.3.3',
+    version='0.3.4',
     license='MIT',
     description='A static HTML blog generator.',
     author='Andreas Lutro',
