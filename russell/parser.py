2f59cc39f66f7aae864e8b530cd9647426daa399
Andreas Lutro <andreas@lutro.me>
2016-02-28 18:53:32 +0100

    prevent kwargs from carrying over from post to post

--- russell/parser.py
+++ russell/parser.py
@@ -19,21 +19,21 @@ def _list_files(root_dir):
 
 
 def read_pages(path, root_url):
-	kwargs = {'root_url': root_url}
 	pages = []
 	LOG.debug('looking for page files in %s', path)
 	for file in _list_files(path):
-		page = russell.markdown.from_file(file, russell.content.Page, kwargs)
+		page = russell.markdown.from_file(file, russell.content.Page,
+			kwargs={'root_url': root_url})
 		pages.append(page)
 	return pages
 
 
 def read_posts(path, root_url):
-	kwargs = {'root_url': root_url}
 	posts = []
 	LOG.debug('looking for post files in %s', path)
 	for file in _list_files(path):
-		post = russell.markdown.from_file(file, russell.content.Post, kwargs)
+		post = russell.markdown.from_file(file, russell.content.Post,
+			kwargs={'root_url': root_url})
 		posts.append(post)
 
 	posts.sort(key=lambda post: post.title)
