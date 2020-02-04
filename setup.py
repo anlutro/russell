efbd816dd79149292c273228116e5db0873037bf
Andreas Lutro <anlutro@gmail.com>
2020-02-04 13:41:46 +0100

    setup.py as well

--- setup.py
+++ setup.py
@@ -10,34 +10,33 @@ if sys.version_info[0] != 3:
     print("Only Python 3 is supported!")
     sys.exit(1)
 
-def read_requirements(path='requirements.txt'):
+
+def read_requirements(path="requirements.txt"):
     fullpath = os.path.join(os.path.dirname(__file__), path)
-    with open(fullpath, 'r') as f:
+    with open(fullpath, "r") as f:
         lines = f.readlines()
     lines = [l.strip() for l in lines if l]
     return [l for l in lines if l]
 
-def get_version(path='russell/__version__.py'):
+
+def get_version(path="russell/__version__.py"):
     with open(path) as fh:
         for line in fh:
-            if line.startswith('__version__ ='):
+            if line.startswith("__version__ ="):
                 return ast.literal_eval(line[14:])
 
+
 setuptools.setup(
-    name='russell',
-    packages=['russell'],
+    name="russell",
+    packages=["russell"],
     version=get_version(),
-    license='GPL-3.0',
-    description='A static HTML blog generator.',
-    author='Andreas Lutro',
-    author_email='anlutro@gmail.com',
-    url='https://github.com/anlutro/russell',
-    keywords=['blog', 'static', 'html', 'generator'],
+    license="GPL-3.0",
+    description="A static HTML blog generator.",
+    author="Andreas Lutro",
+    author_email="anlutro@gmail.com",
+    url="https://github.com/anlutro/russell",
+    keywords=["blog", "static", "html", "generator"],
     classifiers=[],
     install_requires=read_requirements(),
-    entry_points={
-        'console_scripts': [
-            'russell=russell.cli:main'
-        ],
-    },
+    entry_points={"console_scripts": ["russell=russell.cli:main"],},
 )
